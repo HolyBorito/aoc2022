971a8d320afe8e8c3eaf16588570550e9ff8c652
Boris Gesbert <boris.gesbert@ns.nl>
2022-12-02 08:38:02 +0000

    Add new lines

--- day01/solution.py
+++ day01/solution.py
@@ -10,13 +10,13 @@ elf_calories = [list(g) for k, g in groupby(input, key=bool) if k]
 elf_calories = [[int(i) for i in lst] for lst in elf_calories]
 elf_calories_tot = [sum(lst) for lst in elf_calories]
 print(
-    "Amount of calories of the elf carrying the most calories:",
+    "Amount of calories of the elf carrying the most calories:\n",
     max(elf_calories_tot),
 )
 
 # Extract the top three elves with the most calories and their sum
 elf_calories_tot_top3 = sorted(elf_calories_tot, reverse=True)[:3]
 print(
-    "Amount of calories of the top three elves carrying the most calories:",
+    "Amount of calories of the top three elves carrying the most calories:\n",
     sum(elf_calories_tot_top3),
 )
