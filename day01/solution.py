5947241aa188f2a7839bcaa69028dc4a35f6b969
Boris Gesbert <boris.gesbert@ns.nl>
2022-12-08 20:12:37 +0000

    Let the code speak for itself

--- day01/solution.py
+++ day01/solution.py
@@ -4,8 +4,6 @@ from itertools import groupby
 with open(os.getcwd() + "/day01/input.txt") as f:
     input = f.read().splitlines()
 
-# Divide list into sub-lists, convert strings to integers,
-# sum over the values of each sub-list and find the maximum value
 elf_calories = [list(g) for k, g in groupby(input, key=bool) if k]
 elf_calories = [[int(i) for i in lst] for lst in elf_calories]
 elf_calories_tot = [sum(lst) for lst in elf_calories]
@@ -14,7 +12,6 @@ print(
     max(elf_calories_tot),
 )
 
-# Extract the top three elves with the most calories and their sum
 elf_calories_tot_top3 = sorted(elf_calories_tot, reverse=True)[:3]
 print(
     "Amount of calories of the top three elves carrying the most calories:\n",
