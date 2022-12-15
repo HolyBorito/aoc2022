05ca4b85b1b4f59bec2aa800ad253690ede5f08e
Boris Gesbert <boris.gesbert@ns.nl>
2022-12-15 14:32:43 +0000

    Improved parameter naming

--- day04/solution.py
+++ day04/solution.py
@@ -6,15 +6,15 @@ with open(os.getcwd() + "/day04/input.txt") as f:
 
 pairs = [re.split(",|-", str) for str in input]
 
-pair_overlap = {}
-pair_overlap[1] = [
+range_overlap = {}
+range_overlap[1] = [
     True
     if (int(lst[0]) <= int(lst[2]) and int(lst[1]) >= int(lst[3]))
     or (int(lst[0]) >= int(lst[2]) and int(lst[1]) <= int(lst[3]))
     else False
     for lst in pairs
 ]
-pair_overlap[2] = [
+range_overlap[2] = [
     True
     if (int(lst[1]) >= int(lst[2]) and int(lst[1]) <= int(lst[3]))
     or (int(lst[3]) >= int(lst[0]) and int(lst[3]) <= int(lst[1]))
@@ -22,9 +22,9 @@ pair_overlap[2] = [
     for lst in pairs
 ]
 
-for k, lst in pair_overlap.items():
+for k, lst in range_overlap.items():
     print(
-        "The number of overlapping pairs for part",
+        "The number of pairs in which their ranges overlap for part",
         k,
         ":\n",
         sum(lst),
